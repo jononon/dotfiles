f4d8f433bcaf7e65a960e59e4c918c76558e769f
Jonathan Damico <jj_damico@yahoo.com>
2021-07-13 14:38:09 +0100

    Add flags to install file

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo "==== Installing ZSH ===="
+
 # Detect machine
 unameOut="$(uname -s)"
 case "${unameOut}" in
@@ -36,21 +38,34 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 # Fix permissions
 chmod 700 ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+echo "==== Setting ZSH as default shell ===="
+
 chsh -s $(which zsh)
+
+echo "==== Installing P10K ===="
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
         # ...
-        sudo apt install python3-dev python3-pip python3-setuptools
-        sudo pip3 install thefuck
+        echo "==== Installing fasd ===="
         sudo add-apt-repository ppa:aacebedo/fasd
         sudo apt-get update
         sudo apt-get install fasd
+
+        echo "==== Installing Python ===="
+        sudo apt install python3-dev python3-pip python3-setuptools
+
+        echo "==== Installing thefuck ===="
+        sudo pip3 install thefuck
+
 elif [[ "$OSTYPE" == "darwin"* ]]; then
         # Mac OSX
+        echo "==== Installing brew ===="
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        echo "==== Installing fasd ===="
         brew install fasd
+        echo "==== Installing thefuck ===="
         brew install thefuck
 elif [[ "$OSTYPE" == "cygwin" ]]; then
         # POSIX compatibility layer and Linux environment emulation for Windows
@@ -64,6 +79,10 @@ else
         # Unknown.
 fi
 
-cp .jonathan_damico_custom_commands.sh $HOME/.jonathan_damico_custom_commands.sh
-cp .p10k.zsh $HOME/.p10k.zsh
+echo "==== Installing zshrc ===="
 cp .zshrc $HOME/.zshrc
+echo "==== Installing custom comands ===="
+cp .jonathan_damico_custom_commands.sh $HOME/.jonathan_damico_custom_commands.sh
+echo "==== Installing p10k config ===="
+cp .p10k.zsh $HOME/.p10k.zsh
+
