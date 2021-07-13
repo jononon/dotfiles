a3a7686ba0bafa00a463eda03a68d843880f787a
Jonathan Damico <jj_damico@yahoo.com>
2021-07-13 14:42:27 +0100

    get rid of empty elifs

--- install.sh
+++ install.sh
@@ -67,16 +67,16 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
         brew install fasd
         echo "==== Installing thefuck ===="
         brew install thefuck
-elif [[ "$OSTYPE" == "cygwin" ]]; then
-        # POSIX compatibility layer and Linux environment emulation for Windows
-elif [[ "$OSTYPE" == "msys" ]]; then
-        # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
-elif [[ "$OSTYPE" == "win32" ]]; then
-        # I'm not sure this can happen.
-elif [[ "$OSTYPE" == "freebsd"* ]]; then
-        # ...
-else
-        # Unknown.
+# elif [[ "$OSTYPE" == "cygwin" ]]; then
+#         # POSIX compatibility layer and Linux environment emulation for Windows
+# elif [[ "$OSTYPE" == "msys" ]]; then
+#         # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+# elif [[ "$OSTYPE" == "win32" ]]; then
+#         # I'm not sure this can happen.
+# elif [[ "$OSTYPE" == "freebsd"* ]]; then
+#         # ...
+# else
+#         # Unknown.
 fi
 
 echo "==== Installing zshrc ===="
